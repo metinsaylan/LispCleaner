64d3fb1e53fabdc555dccb02b72b197482999bc6
Metin Şaylan <metinsaylan@gmail.com>
2017-08-27 00:18:47 +0300

    add

---
+++ LspCleanerFree/LspCleaner/LspCleaner.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LspCleaner
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.folder = New System.Windows.Forms.FolderBrowserDialog()
+        Me.btnScan = New System.Windows.Forms.Button()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.progress = New System.Windows.Forms.ProgressBar()
+        Me.btnScanFolder = New System.Windows.Forms.Button()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.lblAction = New System.Windows.Forms.Label()
+        Me.lblCancel = New System.Windows.Forms.LinkLabel()
+        Me.chkBackup = New System.Windows.Forms.CheckBox()
+        Me.txtLog = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnScan
+        '
+        Me.btnScan.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnScan.BackColor = System.Drawing.Color.FromArgb(CType(CType(125, Byte), Integer), CType(CType(197, Byte), Integer), CType(CType(54, Byte), Integer))
+        Me.btnScan.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnScan.ForeColor = System.Drawing.Color.White
+        Me.btnScan.Location = New System.Drawing.Point(519, 13)
+        Me.btnScan.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnScan.Name = "btnScan"
+        Me.btnScan.Size = New System.Drawing.Size(123, 38)
+        Me.btnScan.TabIndex = 0
+        Me.btnScan.Text = "Auto Scan"
+        Me.btnScan.UseVisualStyleBackColor = False
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(10, 373)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(183, 18)
+        Me.LinkLabel1.TabIndex = 1
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "LispCleaner by MetinSaylan"
+        Me.LinkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'progress
+        '
+        Me.progress.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.progress.Location = New System.Drawing.Point(11, 348)
+        Me.progress.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.progress.Name = "progress"
+        Me.progress.Size = New System.Drawing.Size(631, 20)
+        Me.progress.TabIndex = 3
+        '
+        'btnScanFolder
+        '
+        Me.btnScanFolder.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnScanFolder.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.btnScanFolder.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnScanFolder.ForeColor = System.Drawing.SystemColors.ControlDark
+        Me.btnScanFolder.Location = New System.Drawing.Point(375, 13)
+        Me.btnScanFolder.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnScanFolder.Name = "btnScanFolder"
+        Me.btnScanFolder.Size = New System.Drawing.Size(138, 38)
+        Me.btnScanFolder.TabIndex = 4
+        Me.btnScanFolder.Text = "Scan Folder"
+        Me.btnScanFolder.UseVisualStyleBackColor = False
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.ForeColor = System.Drawing.Color.DimGray
+        Me.lblStatus.Location = New System.Drawing.Point(10, 23)
+        Me.lblStatus.Margin = New System.Windows.Forms.Padding(0)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(174, 18)
+        Me.lblStatus.TabIndex = 5
+        Me.lblStatus.Text = "Please choose an action.."
+        '
+        'lblAction
+        '
+        Me.lblAction.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lblAction.AutoSize = True
+        Me.lblAction.Location = New System.Drawing.Point(10, 321)
+        Me.lblAction.Margin = New System.Windows.Forms.Padding(0)
+        Me.lblAction.Name = "lblAction"
+        Me.lblAction.Size = New System.Drawing.Size(59, 18)
+        Me.lblAction.TabIndex = 6
+        Me.lblAction.Text = "Ready.."
+        '
+        'lblCancel
+        '
+        Me.lblCancel.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblCancel.AutoSize = True
+        Me.lblCancel.Location = New System.Drawing.Point(554, 55)
+        Me.lblCancel.Name = "lblCancel"
+        Me.lblCancel.Size = New System.Drawing.Size(50, 18)
+        Me.lblCancel.TabIndex = 7
+        Me.lblCancel.TabStop = True
+        Me.lblCancel.Text = "Cancel"
+        Me.lblCancel.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.lblCancel.Visible = False
+        '
+        'chkBackup
+        '
+        Me.chkBackup.AutoSize = True
+        Me.chkBackup.Checked = True
+        Me.chkBackup.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.chkBackup.Location = New System.Drawing.Point(11, 55)
+        Me.chkBackup.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.chkBackup.Name = "chkBackup"
+        Me.chkBackup.Size = New System.Drawing.Size(345, 22)
+        Me.chkBackup.TabIndex = 8
+        Me.chkBackup.Text = "Backup lisp files before cleaning (Recommended)"
+        Me.chkBackup.UseVisualStyleBackColor = True
+        '
+        'txtLog
+        '
+        Me.txtLog.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtLog.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.txtLog.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtLog.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.txtLog.ForeColor = System.Drawing.Color.GreenYellow
+        Me.txtLog.Location = New System.Drawing.Point(11, 84)
+        Me.txtLog.Multiline = True
+        Me.txtLog.Name = "txtLog"
+        Me.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtLog.Size = New System.Drawing.Size(631, 225)
+        Me.txtLog.TabIndex = 9
+        '
+        'LspCleaner
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(654, 398)
+        Me.Controls.Add(Me.txtLog)
+        Me.Controls.Add(Me.chkBackup)
+        Me.Controls.Add(Me.lblCancel)
+        Me.Controls.Add(Me.lblAction)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.btnScanFolder)
+        Me.Controls.Add(Me.progress)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.btnScan)
+        Me.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.MinimumSize = New System.Drawing.Size(515, 370)
+        Me.Name = "LspCleaner"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Lisp Cleaner (Free Version) - MetinSaylan.Com"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents folder As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents btnScan As System.Windows.Forms.Button
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents progress As System.Windows.Forms.ProgressBar
+    Friend WithEvents btnScanFolder As System.Windows.Forms.Button
+    Friend WithEvents lblStatus As System.Windows.Forms.Label
+    Friend WithEvents lblAction As System.Windows.Forms.Label
+    Friend WithEvents lblCancel As System.Windows.Forms.LinkLabel
+    Friend WithEvents chkBackup As System.Windows.Forms.CheckBox
+    Friend WithEvents txtLog As System.Windows.Forms.TextBox
+End Class
---
+++ LspCleanerFree/LspCleaner/LspCleaner.vb
@@ -0,0 +1,351 @@
+﻿Imports System.IO
+Imports System.Text.RegularExpressions
+Imports System.Windows.Forms
+
+Public Class LspCleaner
+
+    Public _log As New List(Of String)
+    Public Shared _cancel As Boolean
+    Private autoScanComplete As Boolean
+    Public backupFiles As Boolean
+
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        autoScanComplete = False
+        Me.backupFiles = True
+    End Sub
+
+    Public Sub Log(ByVal msg As String)
+        _log.Add(msg)
+        If Me.Visible Then
+            txtLog.Text = String.Join(vbCrLf, _log.ToArray)
+
+            txtLog.SelectionStart = txtLog.Text.Length
+            txtLog.ScrollToCaret()
+            Application.DoEvents()
+
+        End If
+    End Sub
+
+    Public Sub Status(ByVal msg As String)
+        lblStatus.Text = msg
+        Me.Refresh()
+    End Sub
+
+    Public Sub Action(ByVal msg As String)
+        lblAction.Text = msg
+        Me.Refresh()
+    End Sub
+
+    Private Sub resetCancel()
+        lblCancel.Visible = False
+        _cancel = False
+        Me.Refresh()
+    End Sub
+
+    Private Sub hideCancel()
+        lblCancel.Visible = False
+    End Sub
+
+    Public Sub AutoClear(verboseFlag As Boolean)
+
+        Dim folders As New List(Of String)
+        Dim found As Boolean = False
+
+        Dim desktop As String = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+
+        Status("Generating folder list")
+        Action("Scanning Progam Files folders..")
+
+        progress.Style = Windows.Forms.ProgressBarStyle.Marquee
+
+        ' Programlar
+        'Dim programsFolder As String
+        'Dim progDir As DirectoryInfo
+        'Dim autocadDirs As DirectoryInfo()
+        'programsFolder = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)
+        'progDir = New DirectoryInfo(programsFolder)
+        'autocadDirs = progDir.GetDirectories("Auto*")
+        'For Each autocadFolder In autocadDirs
+        '    folders.Add(autocadFolder.FullName)
+        'Next
+
+        'Action("Adding special folders..")
+        '' App data
+        'folders.Add(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData))
+        '' Local App data 
+        'folders.Add(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData))
+
+        ' Belgelerim
+        'folders.Add(Environment.GetFolderPath(Environment.SpecialFolder.userprofile))
+        folders.Add(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments))
+
+        'Desktop
+        folders.Add(desktop)
+
+        Status("Generating lisp file list")
+
+        Log("> Scanning lisp files..")
+        Log("")
+
+        resetCancel()
+        ' Scan files
+        Dim lspfiles As New List(Of String)
+        For Each folder As String In folders
+
+            If Not _cancel Then
+                Dim folderInfo As New DirectoryInfo(folder)
+
+                Log("> Scanning Folder : " & folder)
+                Action("Scanning : " & folderInfo.Name)
+
+                Dim fileList As New List(Of String)
+                Dim mnlList As New List(Of String)
+                Dim fasList As New List(Of String)
+                fileList = FileHelper.GetFilesRecursive(folder, "*.lsp")
+                mnlList = FileHelper.GetFilesRecursive(folder, "*.mnl")
+                fasList = FileHelper.GetFilesRecursive(folder, "*.fas")
+
+                For Each fileName As String In fileList
+                    lspfiles.Add(fileName)
+                    Log(fileName)
+                Next
+
+                For Each fileName As String In mnlList
+                    lspfiles.Add(fileName)
+                    Log(fileName)
+                Next
+
+                For Each fileName As String In fasList
+                    lspfiles.Add(fileName)
+                    Log(fileName)
+                Next
+            Else
+                Exit Sub
+            End If
+
+        Next
+
+        Status("Checking lisp file data")
+
+        Log("")
+        Log("> Clearing lisp files..")
+        Log("")
+
+        Dim cntr As Integer = 0
+        found = ClearLispFiles(lspfiles, cntr, Me.backupFiles)
+
+        hideCancel()
+
+        If found Then
+            Log(cntr & " virus cleared.")
+        Else
+            Log("No virus found.")
+        End If
+
+        Log("")
+        Log("> LispCleaner Finished.")
+        Log("")
+
+        Action("")
+        Status("Cleaner finished.")
+
+        btnScan.Text = "ReScan"
+
+        If Not Me.Visible And verboseFlag = True Then
+
+            ' Save log on desktop
+            Dim logData As String = String.Join(vbCrLf, _log.ToArray)
+            FileIO.FileManager.StringToFile(logData, desktop & "\lspcleaner.txt")
+
+            ' Action("Log saved to ""lspcleaner"" on your desktop.")
+            ' btnScan.Text = "View Log.."
+
+        End If
+
+        autoScanComplete = True
+    End Sub
+
+    Public Function ClearLispFiles(ByVal filelist As List(Of String), ByRef virusCount As Integer, Optional ByVal backupFiles As Boolean = True)
+        Dim found As Boolean = False
+
+        If filelist.Count > 0 Then
+
+            progress.Style = Windows.Forms.ProgressBarStyle.Blocks
+            progress.Maximum = filelist.Count - 1
+
+            Dim cntr As Integer = 0
+
+            ' Clean files
+            For Each filepath In filelist
+                If Not _cancel Then
+                    progress.Value = cntr
+                    cntr = cntr + 1
+                    If ClearFile(filepath, Me.backupFiles) = True Then
+                        found = True
+                        virusCount = virusCount + 1
+                    End If
+                Else
+                    Return False
+                    Exit Function
+                End If
+            Next
+
+        End If
+
+        Return found
+    End Function
+
+    ' Returns true if 
+    Private Function ClearFile(filepath As String, backup As Boolean) As Boolean
+        Dim found As Boolean
+        Dim filename As String = Path.GetFileName(filepath)
+
+        If filename = "acaddoc.lsp" Then
+
+            found = True
+            File.Delete(filepath)
+            Log("  Deleting : " & filepath)
+
+        ElseIf filename = "acad.fas" Then
+
+            found = True
+            File.Delete(filepath)
+            Log("  Deleting : " & filepath)
+
+        ElseIf filename = "acadapq.lsp" Then
+
+            found = True
+            File.Delete(filepath)
+            Log("  Deleting : " & filepath)
+
+        ElseIf filename = "textdit.lsp" Then
+
+            found = True
+            File.Delete(filepath)
+            Log("  Deleting : " & filepath)
+
+        Else
+
+            Dim fi As New FileInfo(filepath)
+            Dim filedata As String = FileIO.FileManager.FileToString(filepath)
+
+            Action("Checking : " & fi.Name)
+
+            Dim flagx As Boolean = CheckFlagX(filedata)
+            Dim sstartup As Boolean = CheckSStartup(filedata)
+
+            If flagx Or sstartup Then
+                found = True
+            End If
+
+            If found Then
+
+                Log("  Found virus -> " & filepath)
+
+                If backupFiles Then
+                    ' Backup file
+                    Try
+                        IO.File.Copy(filepath, filepath.Replace(".lsp", ".lsp.bak"))
+                    Catch ex As IO.IOException
+                        ' File backup already exists?
+                        ' Keep silent.
+                    End Try
+
+                    Log("   Backup file : " & filepath & ".bak")
+                End If
+
+                FileIO.FileManager.StringToFile(filedata, filepath)
+                Log("   Cleared file.")
+            End If
+
+        End If
+
+        Return found
+    End Function
+
+    Function CheckFlagX(ByRef filedata As String) As Boolean
+        Dim found As Boolean
+        Dim fileparts As String()
+        Dim pattern As String = "\(setq flagx t\)"
+
+        fileparts = Regex.Split(filedata, pattern)
+
+        If fileparts.Length > 1 Then
+            found = True
+            filedata = fileparts(0)
+        End If
+
+        Return found
+    End Function
+
+    Function CheckSStartup(ByRef filedata As String) As Boolean
+        Dim found As Boolean
+        Dim fileparts As String()
+        Dim pattern As String = "(q\(defun-q s::startup)"
+
+        fileparts = Regex.Split(filedata, pattern)
+
+        If fileparts.Length > 1 Then
+            found = True
+            filedata = fileparts(0)
+        End If
+
+        Return found
+    End Function
+
+
+
+
+    Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        System.Diagnostics.Process.Start("http://metinsaylan.com/projects/lspcleaner/")
+    End Sub
+
+    Private Sub btnScan_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnScan.Click
+
+        _log.Clear()
+        txtLog.Text = ""
+
+        AutoClear(True)
+
+    End Sub
+
+    Private Sub btnScanFolder_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnScanFolder.Click
+
+        MsgBox("ScanFolder feature is only available on paid version." & vbCrLf & "If you like this cleaner, please purchase full version", vbOKOnly + vbInformation)
+
+    End Sub
+
+    Private Sub lblCancel_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles lblCancel.Click
+        _cancel = True
+        lblCancel.Text = "Canceled"
+    End Sub
+
+    Private Sub chkBackup_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chkBackup.CheckedChanged
+        Me.backupFiles = chkBackup.Checked
+    End Sub
+
+    Private Sub LspCleaner_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "LispCleaner " & My.Application.Info.Version.Major & "." & My.Application.Info.Version.Minor & "." & My.Application.Info.Version.Build & " by MS (Free Version)"
+
+        txtLog.Text = "" & _
+        vbCrLf & "PLEASE NOTE : " & _
+            "" & vbCrLf & _
+            "This free version of the LispCleaner cleans Documents and Desktop folders only. Your Program files and Application Data folders won't be cleaned." & vbCrLf & _
+            "If you like this software, please purchase full version. Thanks." & vbCrLf & _
+             "" & vbCrLf & _
+        vbCrLf & "WARNING :" & vbCrLf & _
+            "LispCleaner (Free Version) v" & My.Application.Info.Version.Major & "." & My.Application.Info.Version.Minor & "." & My.Application.Info.Version.Build & " is designed to clean all acaddoc.lsp, acad.fas, acadapq.lsp and textdit.lsp files from selected folders in order to get rid of ALS/Bursted.xx worm. Please be informed that your regular lisp files will also be cleaned by removing infected parts. Before running the cleaner, I recommend backing up your important lisp files. " ' & vbCrLf & vbCrLf & _
+        ' vbCrLf & "DISCLAIMER :" & vbCrLf & _
+        ' "" & vbCrLf & _
+        ' "This SOFTWARE PRODUCT is provided by THE PROVIDER ""as is"" and ""with all faults."" THE PROVIDER makes no representations or warranties of any kind concerning the safety, suitability, lack of viruses, inaccuracies, typographical errors, or other harmful components of this SOFTWARE PRODUCT. There are inherent dangers in the use of any software, and you are solely responsible for determining whether this SOFTWARE PRODUCT is compatible with your equipment and other software installed on your equipment. You are also solely responsible for the protection of your equipment and backup of your data, and THE PROVIDER will not be liable for any damages you may suffer in connection with using, modifying, or distributing this SOFTWARE PRODUCT."
+
+
+
+
+
+    End Sub
+End Class
